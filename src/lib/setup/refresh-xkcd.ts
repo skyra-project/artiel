00f81c94a7fa9719e2d523926467b0b6e070d933
Aura Román <kyradiscord@gmail.com>
2023-06-06 16:36:15 +0200

    feat(xkcd): refresh cron and `n + 1` entry fetching

---
+++ src/lib/setup/refresh-xkcd.ts
@@ -0,0 +1,11 @@
+import { refreshComicsFromRemote } from '#lib/utilities/xkcd';
+import { job } from 'cron';
+
+export const xkcdRefresh = job({
+	// cronTime: '0 0 12,13 * * 1-5',
+	cronTime: '0 35 14 * * 1-5',
+	onTick: refreshComicsFromRemote,
+	start: true,
+	unrefTimeout: true,
+	timeZone: 'Etc/UTC'
+});
