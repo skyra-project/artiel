d9e2f8d02562174ede0e03f2559681f01679778b
Aura Román <kyradiscord@gmail.com>
2024-02-08 23:30:38 +0100

    refactor: improved dice

--- src/lib/i18n/LanguageKeys/Commands/Dice.ts
+++ src/lib/i18n/LanguageKeys/Commands/Dice.ts
@@ -11,6 +11,13 @@ export const OptionsPreset = 'commands/dice:optionsPreset';
 
 export const Title = FT<{ dice: string }>('commands/dice:title');
 
-export const PresetDnD5e = T('commands/dice:presetDnD5e');
-export const PresetCthulhu = T('commands/dice:presetCthulhu');
+export const PresetD2 = T('commands/dice:presetD2');
+export const PresetD4 = T('commands/dice:presetD4');
+export const PresetD6 = T('commands/dice:presetD6');
+export const PresetD8 = T('commands/dice:presetD8');
+export const PresetD10 = T('commands/dice:presetD10');
+export const PresetD10Ren = T('commands/dice:presetD10Ren');
+export const PresetD12 = T('commands/dice:presetD12');
+export const PresetD20 = T('commands/dice:presetD20');
+export const PresetD100 = T('commands/dice:presetD100');
 export const PresetFate = T('commands/dice:presetFate');
