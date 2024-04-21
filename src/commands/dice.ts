15f772955c0b134a0230576440733153ca71c595
Aura Román <kyradiscord@gmail.com>
2024-04-21 23:44:15 +0200

    refactor(dice): add `on-critical-*` options

--- src/commands/dice.ts
+++ src/commands/dice.ts
@@ -1,5 +1,4 @@
 import { LanguageKeys } from '#lib/i18n/LanguageKeys';
-import { arrayWith } from '#lib/utilities/array';
 import { isAny } from '#lib/utilities/operators';
 import { HeadingLevel, bold, heading, underscore } from '@discordjs/builders';
 import { Command, RegisterCommand } from '@skyra/http-framework';
@@ -8,6 +7,14 @@ import { MessageFlags } from 'discord-api-types/v10';
 import { randomInt } from 'node:crypto';
 
 const Root = LanguageKeys.Commands.Dice;
+
+enum OnCriticalReroll {
+	Nothing,
+	RerollOnce,
+	RerollTwice,
+	RerollThrice,
+	RerollIndefinitely
+}
 
 @RegisterCommand((builder) =>
 	applyLocalizedBuilder(builder, Root.RootName, Root.RootDescription) //
@@ -35,26 +42,37 @@ const Root = LanguageKeys.Commands.Dice;
 				createSelectMenuChoiceName(Root.SelectionLeast, { value: 'least' })
 			)
 		)
+		.addIntegerOption((builder) =>
+			applyLocalizedBuilder(builder, Root.OptionsOnCriticalSuccess).addChoices(
+				createSelectMenuChoiceName(Root.OnCriticalNothing, { value: OnCriticalReroll.Nothing }),
+				createSelectMenuChoiceName(Root.OnCriticalRerollOnce, { value: OnCriticalReroll.RerollOnce }),
+				createSelectMenuChoiceName(Root.OnCriticalRerollTwice, { value: OnCriticalReroll.RerollTwice }),
+				createSelectMenuChoiceName(Root.OnCriticalRerollThrice, { value: OnCriticalReroll.RerollThrice }),
+				createSelectMenuChoiceName(Root.OnCriticalRerollIndefinitely, { value: OnCriticalReroll.RerollIndefinitely })
+			)
+		)
+		.addIntegerOption((builder) =>
+			applyLocalizedBuilder(builder, Root.OptionsOnCriticalFailure).addChoices(
+				createSelectMenuChoiceName(Root.OnCriticalNothing, { value: OnCriticalReroll.Nothing }),
+				createSelectMenuChoiceName(Root.OnCriticalRerollOnce, { value: OnCriticalReroll.RerollOnce }),
+				createSelectMenuChoiceName(Root.OnCriticalRerollTwice, { value: OnCriticalReroll.RerollTwice }),
+				createSelectMenuChoiceName(Root.OnCriticalRerollThrice, { value: OnCriticalReroll.RerollThrice }),
+				createSelectMenuChoiceName(Root.OnCriticalRerollIndefinitely, { value: OnCriticalReroll.RerollIndefinitely })
+			)
+		)
 		.addBooleanOption((builder) => applyLocalizedBuilder(builder, Root.OptionsHide))
 )
 export class UserCommand extends Command {
-	private readonly Dice4 = '<:d4:1200786491492802681>';
-	private readonly Dice8 = '<:d8:1200786525634437190>';
-	private readonly Dice10 = '<:d10:1200786415005483018>';
-	private readonly Dice12 = '<:d12:1200786447049957457>';
-	private readonly Dice20 = '<:d20:1200786564914085978>';
-	private readonly DicePlus = '<:dice_plus:1200790107893022740>';
-	private readonly DiceMinus = '<:dice_minus:1200790383903375430>';
-	private readonly DiceNeutral = '<:dice_neutral:1200790382540226570>';
-	private readonly CoinHeads = '<:coin_heads:1205271732747894795>';
-	private readonly CoinTails = '<:coin_tails:1205438570576355338>';
-	private readonly CoinFlip = '<:coinflip:1205271734052196402>';
-	private readonly RollingDiceCup = '<:rolling_dice_cup:1200786597105377290>';
-
 	public override async chatInputRun(interaction: Command.ChatInputInteraction, options: Options) {
 		const { faces, rolls, modifier = 0, preset } = options;
 
-		const { results, render, dice, complex } = this.runPreset(faces, rolls, preset);
+		const { results, render, dice, complex } = this.runPreset(
+			faces,
+			rolls,
+			preset,
+			options['on-critical-success'],
+			options['on-critical-failure']
+		);
 
 		const lines = [heading(resolveKey(interaction, Root.Title, { dice }), HeadingLevel.Three)] as string[];
 		if (!(results.length === 1 && !complex)) {
@@ -79,154 +97,185 @@ export class UserCommand extends Command {
 		}
 	}
 
-	private runPreset(faces?: number, rolls?: number, preset?: Preset) {
+	private runPreset(
+		faces: number | undefined,
+		rolls: number | undefined,
+		preset: Preset | undefined,
+		onCriticalSuccess?: OnCriticalReroll,
+		onCriticalFailure?: OnCriticalReroll
+	) {
 		switch (preset) {
 			case 'd2':
-				return this.runPresetCoin(rolls);
+				return this.throwDice({ faces: 2, rolls, onCriticalSuccess, onCriticalFailure, render: renderCoinDice, complex: true });
 			case 'd4':
-				return this.runPresetUndefined(4, rolls);
+				return this.throwDice({ faces: 4, rolls, onCriticalSuccess, onCriticalFailure });
 			case 'd6':
-				return this.runPresetUndefined(6, rolls);
+				return this.throwDice({ faces: 6, rolls, onCriticalSuccess, onCriticalFailure });
 			case 'd8':
-				return this.runPresetUndefined(8, rolls);
+				return this.throwDice({ faces: 8, rolls, onCriticalSuccess, onCriticalFailure });
 			case 'd10':
-				return this.runPresetUndefined(10, rolls);
+				return this.throwDice({ faces: 10, rolls, onCriticalSuccess, onCriticalFailure });
 			case 'd10:ren':
-				return this.runPresetRen10();
+				return this.throwDice({
+					faces: 10,
+					rolls,
+					onCriticalSuccess: OnCriticalReroll.RerollIndefinitely,
+					onCriticalFailure: OnCriticalReroll.RerollOnce,
+					render: renderRen10Dice,
+					complex: true
+				});
 			case 'd12':
-				return this.runPresetUndefined(12, rolls);
+				return this.throwDice({ faces: 12, rolls, onCriticalSuccess, onCriticalFailure });
 			case 'd20:dnd5e':
-				return this.runPresetDnD5e(rolls);
+				return this.throwDice({ faces: 20, rolls, onCriticalSuccess, onCriticalFailure, render: renderDnD5eDice });
 			case 'd100':
-				return this.runPresetCthulhu(rolls);
+				return this.throwDice({
+					faces: 100,
+					rolls,
+					onCriticalSuccess,
+					onCriticalFailure,
+					criticalFailure: 100,
+					criticalSuccess: 1,
+					render: renderCthulhuDice,
+					complex: true
+				});
 			case 'fate':
-				return this.runPresetFate();
+				return this.throwDice({
+					faces: 3,
+					rolls: 4,
+					offset: -2,
+					onCriticalSuccess: OnCriticalReroll.Nothing,
+					onCriticalFailure: OnCriticalReroll.Nothing,
+					render: renderFateDice,
+					complex: true
+				});
 			default:
-				return this.runPresetUndefined(faces, rolls);
+				return this.throwDice({ faces: faces ?? 6, rolls, onCriticalSuccess, onCriticalFailure });
 		}
 	}
 
-	private runPresetUndefined(faces = 6, rolls = 1) {
-		const results = arrayWith(rolls, () => this.runSingleDice(1, faces + 1));
-		const render = results.map((roll) => underscore(roll.toString())).join(' ');
-		return { results, render, dice: this.renderDice(faces), complex: false };
-	}
+	private throwDice(options: ThrowDiceOptions) {
+		const renders = [] as string[];
+		const results = [] as number[];
+		const offset = options.offset ?? 0;
+		const min = 1 + offset;
+		const max = options.faces + 1 + offset;
+		const rolls = options.rolls ?? 1;
+		const criticalFailure = options.criticalFailure ?? min;
+		const criticalSuccess = options.criticalSuccess ?? max - 1;
+		const onCriticalFailure = options.onCriticalFailure ?? OnCriticalReroll.Nothing;
+		const onCriticalSuccess = options.onCriticalSuccess ?? OnCriticalReroll.Nothing;
+		const render = options.render ?? renderDice;
 
-	private runPresetCoin(rolls = 1) {
-		const results = arrayWith(rolls, () => this.runSingleDice(1, 3));
-		const render = results.map((roll) => (roll === 1 ? this.CoinHeads : this.CoinTails)).join(' ');
-		return { results, render, dice: this.CoinFlip, complex: true };
-	}
-
-	private runPresetDnD5e(rolls = 1) {
-		const results = arrayWith(rolls, () => this.runSingleDice(1, 21));
-		const render = results.map((dice) => this.renderDnD5eDice(dice)).join(' ');
-		return { results, render, dice: this.Dice20, complex: false };
-	}
-
-	private runPresetCthulhu(rolls = 1) {
-		const results = arrayWith(rolls, () => this.runSingleDice(1, 101));
-		const render = results.map((dice) => this.renderCthulhuDice(dice)).join(' ');
-		return { results, render, dice: `${this.Dice10}${this.Dice10}`, complex: true };
-	}
-
-	/**
-	 * The Fate system is a system that uses exclusively 4d6, where the faces are:
-	 * - -1: Minus
-	 * -  0: Neutral
-	 * - +1: Plus
-	 */
-	private runPresetFate() {
-		const results = arrayWith(4, () => this.runSingleDice(-1, 2));
-		const render = results.map((dice) => this.renderFateDice(dice)).join(' ');
-		return { results, render, dice: this.RollingDiceCup, complex: true };
-	}
-
-	/**
-	 * The REN system is a system that uses exclusively d10s, but with two special rules:
-	 * - Rolling a 1 is a botch, and the player must roll another d10 and subtract it from the total.
-	 * - Rolling a 10 allows the player to roll another d10 and add it to the total, repeating as many times as they roll a 10.
-	 *
-	 * This system is used in Regnum Ex Nihilo (REN).
-	 */
-	private runPresetRen10() {
-		const first = this.runSingleDice(1, 11);
-
-		// Case for a 1, we roll a single dice which negates the 1, e.g. 1 then 7 = -6
-		if (first === 1) {
-			const second = this.runSingleDice(1, 11);
-			return {
-				results: [first, -second],
-				render: `${bold(underscore(first.toString()))} ${underscore(second.toString())}`,
-				dice: this.Dice10,
-				complex: true
-			};
+		for (let i = 0; i < rolls; i++) {
+			const first = randomInt(min, max);
+			results.push(first);
+			renders.push(render(first, 0));
+			if (first === criticalSuccess) {
+				const attempts = this.getCriticalRerollAttempts(onCriticalSuccess);
+				for (let i = 0; i < attempts; i++) {
+					const next = randomInt(min, max);
+					results.push(next);
+					renders.push(render(next, i + 1));
+					if (next !== criticalSuccess) break;
+				}
+			} else if (first === criticalFailure) {
+				const attempts = this.getCriticalRerollAttempts(onCriticalFailure);
+				for (let i = 0; i < attempts; i++) {
+					const next = randomInt(min, max);
+					results.push(-next);
+					renders.push(render(next, i + 1));
+					if (next !== criticalFailure) break;
+				}
+			}
 		}
 
-		const results = [first];
-
-		// Case for a 10, we roll more dice until we get a non-10:
-		while (results.at(-1) === 10) {
-			results.push(this.runSingleDice(1, 11));
-		}
-
-		const render = results.map((dice, index) => this.renderRen10Dice(dice, index)).join(' ');
-		return { results, render, dice: this.Dice10, complex: false };
+		return { results, render: renders.join(' '), dice: renderDice(options.faces), complex: options.complex ?? false };
 	}
 
-	private runSingleDice(min: number, max: number) {
-		return randomInt(min, max);
-	}
-
-	private renderDice(faces: number) {
-		switch (faces) {
-			case 2:
-				return this.CoinFlip;
-			case 4:
-				return this.Dice4;
-			case 8:
-				return this.Dice8;
-			case 10:
-				return this.Dice10;
-			case 12:
-				return this.Dice12;
-			case 20:
-				return this.Dice20;
-			case 100:
-				return `${this.Dice10}${this.Dice10}`;
-			default:
-				return this.RollingDiceCup;
+	private getCriticalRerollAttempts(type: OnCriticalReroll) {
+		switch (type) {
+			case OnCriticalReroll.Nothing:
+				return 0;
+			case OnCriticalReroll.RerollOnce:
+				return 1;
+			case OnCriticalReroll.RerollTwice:
+				return 2;
+			case OnCriticalReroll.RerollThrice:
+				return 3;
+			case OnCriticalReroll.RerollIndefinitely:
+				return Infinity;
 		}
 	}
+}
 
-	private renderDnD5eDice(dice: number) {
-		const styled = underscore(dice.toString());
-		return isAny(dice, 1, 20) ? bold(styled) : styled;
-	}
+enum DiceEmoji {
+	Dice4 = '<:d4:1200786491492802681>',
+	Dice8 = '<:d8:1200786525634437190>',
+	Dice10 = '<:d10:1200786415005483018>',
+	Dice12 = '<:d12:1200786447049957457>',
+	Dice20 = '<:d20:1200786564914085978>',
+	DicePlus = '<:dice_plus:1200790107893022740>',
+	DiceMinus = '<:dice_minus:1200790383903375430>',
+	DiceNeutral = '<:dice_neutral:1200790382540226570>',
+	CoinHeads = '<:coin_heads:1205271732747894795>',
+	CoinTails = '<:coin_tails:1205438570576355338>',
+	CoinFlip = '<:coinflip:1205271734052196402>',
+	RollingDiceCup = '<:rolling_dice_cup:1200786597105377290>'
+}
 
-	private renderCthulhuDice(dice: number) {
-		const tens = Math.floor((dice % 100) / 10) * 10;
-		const units = dice % 10;
-		return underscore(`${tens.toString().padStart(2, '0')} ${units.toString()}`);
+function renderDice(faces: number) {
+	switch (faces) {
+		case 2:
+			return DiceEmoji.CoinFlip;
+		case 4:
+			return DiceEmoji.Dice4;
+		case 8:
+			return DiceEmoji.Dice8;
+		case 10:
+			return DiceEmoji.Dice10;
+		case 12:
+			return DiceEmoji.Dice12;
+		case 20:
+			return DiceEmoji.Dice20;
+		case 100:
+			return `${DiceEmoji.Dice10}${DiceEmoji.Dice10}`;
+		default:
+			return DiceEmoji.RollingDiceCup;
 	}
+}
 
-	private renderFateDice(dice: number) {
-		switch (dice) {
-			case -1:
-				return this.DiceMinus;
-			case 0:
-				return this.DiceNeutral;
-			case 1:
-				return this.DicePlus;
-			default:
-				return '';
-		}
+function renderFateDice(dice: number) {
+	switch (dice) {
+		case -1:
+			return DiceEmoji.DiceMinus;
+		case 0:
+			return DiceEmoji.DiceNeutral;
+		case 1:
+			return DiceEmoji.DicePlus;
+		default:
+			return '';
 	}
+}
 
-	private renderRen10Dice(dice: number, index: number) {
-		const styled = underscore(dice.toString());
-		return index === 0 ? bold(styled) : styled;
-	}
+function renderCoinDice(dice: number) {
+	return dice === 1 ? DiceEmoji.CoinHeads : DiceEmoji.CoinTails;
+}
+
+function renderRen10Dice(dice: number, index: number) {
+	const styled = underscore(dice.toString());
+	return index === 0 ? bold(styled) : styled;
+}
+
+function renderDnD5eDice(dice: number) {
+	const styled = underscore(dice.toString());
+	return isAny(dice, 1, 20) ? bold(styled) : styled;
+}
+
+function renderCthulhuDice(dice: number) {
+	const tens = Math.floor((dice % 100) / 10) * 10;
+	const units = dice % 10;
+	return underscore(`${tens.toString().padStart(2, '0')} ${units.toString()}`);
 }
 
 interface Options {
@@ -235,7 +284,21 @@ interface Options {
 	modifier?: number;
 	preset?: Preset;
 	selection?: Selection;
+	'on-critical-success'?: OnCriticalReroll;
+	'on-critical-failure'?: OnCriticalReroll;
 	hide?: boolean;
+}
+
+interface ThrowDiceOptions {
+	faces: number;
+	rolls?: number;
+	offset?: number;
+	criticalSuccess?: number;
+	criticalFailure?: number;
+	onCriticalSuccess?: OnCriticalReroll;
+	onCriticalFailure?: OnCriticalReroll;
+	render?: (dice: number, index: number) => string;
+	complex?: boolean;
 }
 
 type Preset = 'd2' | 'd4' | 'd6' | 'd8' | 'd10' | 'd10:ren' | 'd12' | 'd20:dnd5e' | 'd100' | 'fate';
